2374454772755dd5b4d6945f73e173ab85287f3d
Diegoverflow <belardinelli.diego@gmail.com>
2026-03-15 21:19:15 +0100

    new product valiadation

--- src/main/java/com/example/BarsAndRestaurantsApp/domain/dtos/CreateProductRequestDto.java
+++ src/main/java/com/example/BarsAndRestaurantsApp/domain/dtos/CreateProductRequestDto.java
@@ -2,10 +2,7 @@ package com.example.BarsAndRestaurantsApp.domain.dtos;
 
 import com.example.BarsAndRestaurantsApp.domain.entities.entitiesEnums.Allergen;
 import com.example.BarsAndRestaurantsApp.domain.entities.entitiesEnums.ProductCategory;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -23,23 +20,47 @@ public class CreateProductRequestDto {
 
     @NotBlank(message = "Title is required")
     @Size(max = 20, message = "A title can contain {max} characters")
+    @Pattern(
+            regexp = "^[A-Za-zÀ-ÿ ]+$",
+            message = "Title IT can only contain letters and spaces"
+    )
     private String titleIt;
 
     @Size(max = 20, message = "A title can contain {max} characters")
+    @Pattern(
+            regexp = "^[A-Za-zÀ-ÿ ]+$",
+            message = "Title EN can only contain letters and spaces"
+    )
     private String titleEn;
 
     @Size(max = 200, message = "A description can contain {max} characters")
+    @Pattern(
+            regexp = "^[A-Za-zÀ-ÿ0-9 .,;:!?()'\"\\-\\n\\r]+$",
+            message = "Description ITcontains invalid characters"
+    )
     private String descriptionIt;
 
     @Size(max = 200, message = "A description can contain {max} characters")
+    @Pattern(
+            regexp = "^[A-Za-zÀ-ÿ0-9 .,;:!?()'\"\\-\\n\\r]+$",
+            message = "Description EN contains invalid characters"
+    )
     private String descriptionEn;
 
     @NotBlank(message = "Ingredients are required")
-    @Size(max = 50, message = "Ingredients can contain {max} characters")
+    @Size(max = 50, message = "Ingredients IT can contain {max} characters")
+    @Pattern(
+            regexp = "^[a-zA-ZÀ-ÿ ,]+$",
+            message = "Ingredients can only contain letters, spaces and commas"
+    )
     private String ingredientsIt;
 
     @NotBlank(message = "Ingredients are required")
-    @Size(max = 50, message = "Ingredients can contain {max} characters")
+    @Size(max = 50, message = "Ingredients EN can contain {max} characters")
+    @Pattern(
+            regexp = "^[a-zA-ZÀ-ÿ ,]+$",
+            message = "Ingredients can only contain letters, spaces and commas"
+    )
     private String ingredientsEn;
 
     @NotNull(message = "Product category is required")
